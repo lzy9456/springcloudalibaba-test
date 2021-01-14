04490a9f6e541bf7e4fdc014bb3e6cbf53794549
lzy9456@163.com <lzy9456@163.com>
2021-01-14 14:39:48 +0800

    【springaop+log注解】写日志LogAspect - 修改日志显示，添加方法、入参显示 【springaop+log注解】redis缓存操作RedisAspect - 添加TODO redis获取注释，后续加入redis，在具体实现

--- common/src/main/java/com/example/springaop/log/LogAspect.java
+++ common/src/main/java/com/example/springaop/log/LogAspect.java
@@ -1,7 +1,7 @@
 package com.example.springaop.log;
 
 
-import com.example.springaop.redis.RedisAnno;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -20,13 +20,34 @@ public class LogAspect {
     @Around("@annotation(logAnno)")
     public Object doAround(ProceedingJoinPoint point, LogAnno logAnno) throws Throwable {
         String mName = point.getSignature().getName();
-        log.info("************** {} log begin, do something", mName);
+        log.info("************** {} begin, {}", mName, appendObjLog(point.getArgs()));
 
 
         Object obj = point.proceed();
 
-        log.info("**************  end, do something");
+        log.info("************** {} end, {}", mName, appendObjLog(obj));
 
         return obj;
+    }
+
+
+    private String appendObjLog(Object obj) {
+        try {
+            return new StringBuilder(obj.getClass().getSimpleName()).append(" ").append(toJsonStr(obj)).toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("转换json异常",e);
+        }
+        return "null";
+    }
+
+    private String toJsonStr(Object obj) {
+        try {
+            return JSON.toJSONString(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("转换json异常",e);
+        }
+        return "null";
     }
 }
--- common/src/main/java/com/example/springaop/redis/RedisAspect.java
+++ common/src/main/java/com/example/springaop/redis/RedisAspect.java
@@ -20,8 +20,11 @@ public class RedisAspect {
     public Object doAround(ProceedingJoinPoint point, RedisAnno redisAnno) throws Throwable {
         log.info("************** redis begin");
 
+        //TODO 缓存中取，有返回return obj
 
         Object obj = point.proceed();
+
+        //TODO 缓存中没有，查询后更新到缓存，还可以异步or延迟解决数据一致性
 
         log.info("************** redis end");
 
